970d87923b8de9a1187611f6f53fe5f3990d1a47
DebsLorena <lorenahrd@gmail.com>
2022-12-06 09:27:50 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import ChatBot from 'react-simple-chatbot';
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+
+
+const theme = {
+  background: ' #EDECED',
+  fontFamily: 'Helvetica Neue',
+  headerBgColor: '#84012F',
+  headerFontColor: '#fff',
+  headerFontSize: '15px',
+  botBubbleColor: '#84012F',
+  botFontColor: '#fff',
+  userBubbleColor: '#fff',
+  userFontColor: '#4a4a4a',
+};
 
 function App() {
+
+  const steps = [
+    {
+      id: '1',
+      message: 'Oi, qual o seu nome?',
+      trigger: '2',
+    },
+    {
+      id: '2',
+      user: true,
+      trigger: '3',
+    },
+    {
+      id: '3',
+      message: '{previousValue}, seja muito bem vindo! Por favor selecione uma opção a baixo!',
+      trigger: '4',
+    },
+    {
+      id: '4',
+      options: [
+        { value:'Proximos', label: 'Próximos jogos', trigger:'Proximos'},
+        { value:'Tabela', label: 'Tabela', trigger:'Tabela'},
+        { value:'Onde', label: 'Onde Assistir', trigger:'Onde'},
+      ],
+    },
+    {
+      id:'Proximos',
+      message: 'https://ge.globo.com/agenda/#/todos',
+      trigger: '5',
+    },
+    {
+      id:'Tabela',
+      message: 'https://ge.globo.com/futebol/copa-do-mundo/2022/',
+      trigger: '5',
+    },
+    {
+      id:'Onde',
+      message: 'https://www.youtube.com/c/cazetv',
+      trigger: '5',
+    },
+    {
+      id: '5',
+      message: 'Espero ter ajudado, estou a disposição!',
+      end: true,
+    },
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeProvider theme={theme}>
+      <ChatBot steps={steps} />
+      </ThemeProvider>
     </div>
   );
 }
